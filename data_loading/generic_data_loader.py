2cfc15c287d597e3b9f2f8c87bd707d034834310
tantefar <jendal@live.dk>
2019-12-13 16:56:16 +0100

    Added indices loader

--- data_loading/generic_data_loader.py
+++ data_loading/generic_data_loader.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 """
@@ -8,7 +9,7 @@ Loads data almost unmanipulated.
 def load_csv_data_ratings(path, select_columns=None):
     """
     Loads data from a csv file and returns selected columns as a list of lists.
-    :param path: Path to file.
+    :param path: Path to datafile.
     :param select_columns: A list of selected column names.
     :return: A list, with the selected columns as lists.
     """
@@ -18,9 +19,24 @@ def load_csv_data_ratings(path, select_columns=None):
     df = pd.read_csv(path)
     df = df[select_columns]
 
-    return [val.to_list() for _, val in df.iterrows()]
+    return np.array([val.to_numpy() for _, val in df.iterrows()])
+
+
+def load_csv_mindreader_ratings_with_indices(path):
+    """
+    Loads mindreader data ratings and creates index for entities and users
+    :param path: Path to datafile
+    :return: Data, user to index and entity to index
+    """
+
+    data = load_csv_data_ratings(path)
+
+    user_index = {user: i for i, user in enumerate(set(data[:, 0]))}
+    entity_index = {entity: i for i, entity in enumerate(set(data[:, 1]))}
+
+    return data, user_index, entity_index
 
 
 if __name__ == '__main__':
-    a = load_csv_data_ratings('mindreader/ratings.csv')
+    a = load_csv_mindreader_ratings_with_indices('mindreader/ratings.csv')
     print(a[:10])
