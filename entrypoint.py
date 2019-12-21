5d6bf496ee7e9fe1793a1423840c36003366c0e2
Anders Jakobsen <andersjkbsn@gmail.com>
2019-12-21 23:14:47 +0100

    theis FØLG NU CONVENTIONS

--- entrypoint.py
+++ entrypoint.py
@@ -7,37 +7,64 @@ from loguru import logger
 from data_loading.loo_data_loader import DesignatedDataLoader
 from metrics.metrics import ndcg_at_k
 from models.bpr_recommender import BPRRecommender
+from models.item_knn_recommender import ItemKNNRecommender
+from models.pagerank.collaborative_pagerank_recommender import CollaborativePageRankRecommender
+from models.pagerank.kg_pagerank_recommender import KnowledgeGraphPageRankRecommender
 from models.randumb import RandomRecommender
 from models.svd_recommender import SVDRecommender
 from models.top_pop_recommender import TopPopRecommender
+from models.user_knn_recommender import UserKNNRecommender
 
 models = {
     'transe': None,
     'transe-kg': None,
-    'user-knn': None,
-    'item-knn': None,
+    'user-knn': {
+        'class': UserKNNRecommender,
+        'data_loader': True
+    },
+    'item-knn': {
+        'class': ItemKNNRecommender,
+        'data_loader': True
+    },
     'svd': {
-        'constructor': SVDRecommender
+        'class': SVDRecommender
     },
     'bpr': {
-        'constructor': BPRRecommender
+        'class': BPRRecommender
     },
-    'pr-collab': None,
-    'pr-kg': None,
+    'pr-collab': {
+        'class': CollaborativePageRankRecommender
+    },
+    'pr-kg': {
+        'class': KnowledgeGraphPageRankRecommender,
+        'data_loader': True
+    },
     'pr-joint': None,
     'mf': None,
     'joint-mf': None,
     'top-pop': {
-        'constructor': TopPopRecommender
+        'class': TopPopRecommender
     },
     'random': {
-        'constructor': RandomRecommender
+        'class': RandomRecommender
     }
 }
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--include', nargs='*', type=str, choices=models.keys(), help='models to include')
 parser.add_argument('--exclude', nargs='*', type=str, choices=models.keys(), help='models to exclude')
+
+
+def instantiate(parameters, loader):
+    if not dict or 'class' not in parameters:
+        return None
+
+    kwargs = dict()
+    if parameters.get('data_loader', False):
+        kwargs['data_loader'] = loader
+
+    return parameters['class'](**kwargs)
+
 
 if __name__ == '__main__':
     logger.info(f'Working directory: {os.getcwd()}')
@@ -48,41 +75,45 @@ if __name__ == '__main__':
     if args.exclude:
         model_selection = model_selection.difference(set(args.exclude))
 
-    # Load data
-    data_loader = DesignatedDataLoader.load_from(
-        path='data_loading/mindreader',
-        movies_only=False,
-        min_num_entity_ratings=1
-    )
+    for v in [True, False]:
+        logger.info(f'Movies only: {v}')
 
-    train, validation, test = data_loader.make(
-        movie_to_entity_ratio=0.25,
-        replace_movies_with_descriptive_entities=True
-    )
+        # Load data
+        data_loader = DesignatedDataLoader.load_from(
+            path='./data_loading/mindreader',
+            movies_only=v,
+            min_num_entity_ratings=1
+        )
 
-    # Run models
-    for model in model_selection:
-        recommender = models[model]['constructor']()
+        train, validation, test = data_loader.make(
+            movie_to_entity_ratio=1,
+            keep_all_ratings=True
+        )
 
-        recommender.fit(train, validation)
+        # Run models
+        for model in model_selection:
+            recommender = instantiate(models[model], data_loader)
+            if not recommender:
+                logger.error(f'No parameters specified for {model}')
 
-        hits, count = 0, 0
-        ndcg_sum = list()
+                continue
 
-        k = 10
-        for u, (pos_sample, neg_samples) in test:
-            predictions = recommender.predict(u, neg_samples + [pos_sample]).items()
-            predictions = sorted(predictions, key=lambda item: item[1], reverse=True)
+            logger.info(f'Fitting {model}')
+            recommender.fit(train, validation)
 
-            relevance = [1 if item == pos_sample else 0 for item, score in predictions]
-            top_k = [item[0] for item in predictions][:k]
+            hits = dict()
+            ndcg_sum = list()
 
-            if pos_sample in top_k:
-                hits += 1
+            k = 10
+            for u, (pos_sample, neg_samples) in test:
+                predictions = recommender.predict(u, neg_samples + [pos_sample]).items()
+                predictions = sorted(predictions, key=lambda item: item[1], reverse=True)[:k]
 
-            ndcg_sum.append(ndcg_at_k(relevance, 10))
+                relevance = [1 if item == pos_sample else 0 for item, score in predictions]
+                top_k = [item[0] for item in predictions]
 
-            count += 1
+                hits.append(pos_sample in top_k)
+                ndcg_sum.append(ndcg_at_k(relevance, 10))
 
-        logger.info(f'{model} HR: {hits / count * 100:.2f}')
-        logger.info(f'{model} NDCG: {np.mean(ndcg_sum) * 100:.2f}')
+            logger.info(f'{model} HR: {np.mean(hits) * 100:.2f}')
+            logger.info(f'{model} NDCG: {np.mean(ndcg_sum) * 100:.2f}')
