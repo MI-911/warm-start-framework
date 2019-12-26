a1ffa842efc08b844fc4429c00b85e471357df07
Minibrams <anders@brams.dk>
2019-12-26 21:06:40 +0100

    Parallellised data generation, pushing to server

--- data_loading/data_generator.py
+++ data_loading/data_generator.py
@@ -2,6 +2,8 @@ from data_loading.loo_data_loader import DesignatedDataLoader
 from os.path import join
 import json
 import os
+from multiprocessing.pool import Pool
+from time import time
 
 from loguru import logger
 
@@ -138,7 +140,7 @@ def generate_without_top_pop(filter_unkowns=False):
                     args['random_seed'] = random_seed
                     args['without_top_pop'] = True
                     loader = DesignatedDataLoader.load_from(
-                        join('../data_loading', 'mindreader'),
+                        join('./data_loading', 'mindreader'),
                         min_num_entity_ratings=2,
                         filter_unknowns=filter_unkowns,
                         unify_user_indices=False,
@@ -162,9 +164,52 @@ def generate_without_top_pop(filter_unkowns=False):
                     print(e)
                     n_attempts += 1
 
+def _generate_dataset(args): 
+    (experiment, args), (filter_unknowns, without_top_pop, i) = args
+    experiment_dir = join(f'datasets{"" if filter_unknowns else "_with_unknowns"}', experiment)
+    if not os.path.exists(experiment_dir):
+        os.makedirs(experiment_dir)
+
+    successfully_created_experiment = False
+    while not successfully_created_experiment:
+        try:
+            filename = join(experiment_dir, f'{"ntp" if without_top_pop else "wtp"}-{i}.json')
+            random_seed = time()
+            logger.info(f'Attempting to create {filename} with random seed {random_seed}...')
+            args['random_seed'] = random_seed
+            args['without_top_pop'] = without_top_pop
+            loader = DesignatedDataLoader.load_from(
+                join('./data_loading', 'mindreader'),
+                min_num_entity_ratings=1,
+                filter_unknowns=filter_unknowns,
+                unify_user_indices=False,
+                random_seed=random_seed
+            )
+            train, val, test = loader.make(**args)
+            dict_train = []
+            for u, ratings in train:
+                dict_train.append((u, [r.__dict__ for r in ratings]))
+
+            with open(filename, 'w') as fp:
+                json.dump({
+                    'training': dict_train,
+                    'validation': val,
+                    'testing': test
+                }, fp)
+            successfully_created_experiment = True
+
+            logger.info(f'Successfully wrote {filename} to disk.')
+        except AssertionError as e:
+            print(e)
+
+
+
+def generate(filter_unknowns=False, without_top_pop=False, base_dir='./results', n_experiments=10):
+    for i in range(n_experiments):
+        with Pool(8) as p: 
+            p.map(_generate_dataset, [((args), (filter_unknowns, without_top_pop, i)) for args in experiments])
+
 
 if __name__ == '__main__':
-    generate_with_top_pop(filter_unkowns=True)
-    generate_without_top_pop(filter_unkowns=True)
-    generate_with_top_pop(filter_unkowns=False)
-    generate_without_top_pop(filter_unkowns=False)
+    generate(filter_unknowns=True, without_top_pop=True, base_dir='./datasets')
+    generate(filter_unknowns=True, without_top_pop=False, base_dir='./datasets')
