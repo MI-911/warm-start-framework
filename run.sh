4e985a2541fd02a225d76fa317857f8247ded2ad
Anders Langballe Jakobsen <andersjkbsn@gmail.com>
2020-01-25 13:06:04 +0100

    Remove containers after running

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@ git pull
 docker build -t mi911/runner .
 
 #wtp
-docker run -d -v ${PWD}/.data:/app/data -v ${PWD}/results:/app/results mi911/runner --include bpr svd --experiments wtp-all_movies --debug
-docker run -d -v ${PWD}/.data:/app/data -v ${PWD}/results:/app/results mi911/runner --include bpr svd --experiments wtp-all_entities --debug
+docker run --rm -d -v ${PWD}/.data:/app/data -v ${PWD}/results:/app/results mi911/runner --include bpr svd --experiments wtp-all_movies --debug
+docker run --rm -d -v ${PWD}/.data:/app/data -v ${PWD}/results:/app/results mi911/runner --include bpr svd --experiments wtp-all_entities --debug
 
 #ntp
-docker run -d -v ${PWD}/.data:/app/data -v ${PWD}/results:/app/results mi911/runner --include bpr svd --experiments ntp-all_movies --debug
-docker run -d -v ${PWD}/.data:/app/data -v ${PWD}/results:/app/results mi911/runner --include bpr svd --experiments ntp-all_entities --debug
+docker run --rm -d -v ${PWD}/.data:/app/data -v ${PWD}/results:/app/results mi911/runner --include bpr svd --experiments ntp-all_movies --debug
+docker run --rm -d -v ${PWD}/.data:/app/data -v ${PWD}/results:/app/results mi911/runner --include bpr svd --experiments ntp-all_entities --debug
