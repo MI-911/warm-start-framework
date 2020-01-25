9248df3650d38c1d53c334aff2b9173580f79f37
Anders Jakobsen <andersjkbsn@gmail.com>
2020-01-25 17:58:27 +0100

    ML converter

--- data_generation_entry.py
+++ data_generation_entry.py
@@ -2,5 +2,5 @@ from data_loading.data_generator import generate
 
 
 if __name__ == '__main__': 
-    generate(n_experiments=10, filter_unknowns=True, without_top_pop=False, base_dir='./datasets')
+    generate(n_experiments=5, filter_unknowns=True, without_top_pop=False, base_dir='.ml_data')
     # generate(n_experiments=10, filter_unknowns=True, without_top_pop=True, base_dir='./results')
